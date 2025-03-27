bc80aa060acec2834b7daa3f677754dbaf0bc393
Aneesh Kolar <kolaraneesh2003@gmail.com>
2025-03-27 21:27:20 +0530

    Added changes

--- src/config/logger-config.js
+++ src/config/logger-config.js
@@ -1,10 +1,10 @@
 const { createLogger, format, transports } = require("winston");
-const { combine, timestamp, label, prettyPrint } = format;
+const { combine, timestamp, label, printf } = format;
 
-const customFormat = printf((level, message, label, timestamp) => {
-  return `${timestamp} : [${label}] : ${level}: ${message}`;
-});
-
+const customFormat = printf(({ level, message, timestamp }) => {
+    return `${timestamp} : ${level}: ${message}`;
+  });
+  
 const logger = createLogger({
   format: combine(
     label({ label: "right meow!" }),
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const apiRoutes = require('./routes');
+const express = require("express");
+const apiRoutes = require("./routes");
 
-const {ServerConfig} = require('./config/index.js');
+const { ServerConfig, Logger } = require("./config/index.js");
 
 const app = express();
 
-app.use('/api', apiRoutes);sdfasd
+app.use("/api", apiRoutes);
 
-app.listen(ServerConfig.PORT, (req, res) => {
-    console.log(`Server running on port ${ServerConfig.PORT}`);
-})
-
+app.listen(ServerConfig.PORT, () => {
+  console.log(`Server running on port ${ServerConfig.PORT}`);
+  Logger.info("Successfully Started the server", {});
+});
